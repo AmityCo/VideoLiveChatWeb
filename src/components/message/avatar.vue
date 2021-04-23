0843be1bf44ac38c8ad0dbf2635d4763f1026253
supakorn <supakorn@amity.co>
2021-04-23 10:09:21 +0700

    separated SCSS File

--- src/components/message/avatar.vue
+++ src/components/message/avatar.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="Avatar">
     <figure class="image is-24x24">
-       <img
+      <img
         class="is-rounded"
-        :src="isAvatar ? user.model.avatarFileId : require('@/assets/avatar_placeholder.svg')"
+        :src="
+          isAvatar
+            ? user.model.avatarFileId
+            : require('@/assets/avatar_placeholder.svg')
+        "
         alt="avatar"
       />
     </figure>
