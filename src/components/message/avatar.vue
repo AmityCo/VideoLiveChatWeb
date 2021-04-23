26fe96944713a9022231485074f9d7c0197c0aec
supakorn <supakorn@amity.co>
2021-04-23 14:10:07 +0700

    fixed for compat with codesnadbox

--- src/components/message/avatar.vue
+++ src/components/message/avatar.vue
@@ -3,11 +3,7 @@
     <figure class="image is-24x24">
       <img
         class="is-rounded"
-        :src="
-          isAvatar
-            ? user.model.avatarFileId
-            : require('@/assets/avatar_placeholder.svg')
-        "
+        :src="isAvatar ? user.model.avatarFileId : placeholder_img"
         alt="avatar"
       />
     </figure>
@@ -21,5 +17,8 @@ export default {
   computed: {
     isAvatar: ({ user }) => user?.model?.avatarFileId,
   },
+  data: () => ({
+    placeholder_img: require("@/assets/avatar_placeholder.svg"),
+  }),
 };
 </script>
