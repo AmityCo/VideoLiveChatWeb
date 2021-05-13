5e9a54ec537fcf433e286b9d69ada2be8080c34e
supakorn <supakorn@amity.co>
2021-05-13 17:56:59 +0700

    Delete chatbox_bus.js

--- src/chatbox/chatbox_bus.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import EkoClient from "eko-sdk";
-
-export const chatbox_bus = new Vue();
-
-chatbox_bus.$on("api_key_updated", (key) => {
-  const client = EkoClient.create({ apiKey: key });
-  Vue.prototype.$client = client;
-});
