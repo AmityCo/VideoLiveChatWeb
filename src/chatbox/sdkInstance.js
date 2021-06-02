49580c742a8dee03e33a8937adce88b466f0bdcf
Julien Barbay <julien.barbay@gmail.com>
2021-06-02 22:41:52 +0700

    trimming the fat on the client + onConnect callback

--- src/chatbox/sdkInstance.js
+++ src/chatbox/sdkInstance.js
@@ -2,35 +2,30 @@ import ASCClient from "@amityco/js-sdk";
 
 export const ClientInstance = (() => {
   // Private Interface
-  let _client;
-  let _apikey;
-  let _userId;
-
-  function isClientExist() {
-    if (typeof _client != "undefined") {
-      return Object.keys(_client).length > 0;
-    }
-    return false;
-  }
+  let client;
 
   // Public Interface
   return {
-    init(key) {
-      if (!isClientExist() && _apikey !== key) {
-        _apikey = key;
-        _client = new ASCClient({ apiKey: _apikey });
+    init(apiKey) {
+      if (!client) {
+        client = new ASCClient({ apiKey });
       }
-      return;
     },
-    registerUserSession: function (userId) {
-      _userId = userId;
-      _client.registerSession({ userId: _userId });
+
+    registerUserSession: function (userId, onConnect = () => {}) {
+      client.registerSession({ userId });
+
+      client.once("connectionStatusChanged", ({ newValue }) => {
+        onConnect(newValue);
+      });
     },
+
     isInitial() {
-      return isClientExist();
+      return !!client;
     },
+
     getUserID() {
-      return _userId;
+      return client.currentUser.userId;
     },
   };
 })();
