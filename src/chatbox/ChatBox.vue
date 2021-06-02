e97a39648f0e4f6d2c05dad1ed17260b1a9a8ff6
Julien Barbay <julien.barbay@gmail.com>
2021-06-02 22:42:09 +0700

    fixing the component (join channel issue)

--- src/chatbox/ChatBox.vue
+++ src/chatbox/ChatBox.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="ChatBox">
-    <!-- <chat-box-header/> -->
-    <chat-box-message-list
-      v-if="isJoined"
-      :channelId="channelId"
-      @messages_found="stopJoinChannel()"
-      @messages_not_found="recallJoinChannel()"
-    />
+    <chat-box-message-list v-if="channel" :channelId="channelId" />
     <chat-box-input :channelId="channelId" />
   </div>
 </template>
@@ -24,63 +18,68 @@ import { CHATBOX_STYLE } from "@/chatbox/config";
 
 export default {
   name: "ChatBox",
+
   components: {
     ChatBoxInput,
     ChatBoxMessageList,
   },
+
   provide: {
     font_color: CHATBOX_STYLE.font_color,
     chat_color: CHATBOX_STYLE.chat_color,
     composer_color: CHATBOX_STYLE.composer_color,
   },
-  props: ["api_key", "userId", "channelId"],
+
+  props: ["apiKey", "userId", "channelId"],
+
   data: () => ({
+    channel: null,
+
     isJoined: false,
     isFoundMessages: false,
   }),
+
   watch: {
     channelId: function (newVal) {
-      this.joinUserToChannel(newVal, ChannelType.Standard);
+      this.joinUserToChannel(newVal);
     },
   },
-  methods: {
-    stopJoinChannel() {
-      console.log("stop-joinUserToChannel");
-      this.isFoundMessages = true;
-    },
-    recallJoinChannel() {
-      if (!this.isFoundMessages) {
-        console.log("recall-joinUserToChannel");
-        setTimeout(
-          this.joinUserToChannel(this.channelId, ChannelType.Standard),
-          500
-        );
-      }
-    },
-    joinUserToChannel(channelId, type) {
-      console.log("start-joinUserToChannel");
-      this.isJoined = false;
-      channelRepo.joinChannel({ channelId, type });
-      this.isJoined = true;
 
-      // const liveChannel = channelRepo.joinChannel({ channelId, type });
-      // const callback = (data) => {
-      //   this.isJoined = true;
-      //   console.log("joinChannel dataUpdated:", data);
-      // };
-      // if (!liveChannel.model) {
-      //   this.isJoined = true;
-      //   liveChannel.once("dataUpdated", callback);
-      //   // liveChannel.once("dataError", ( err ) => console.log(err));
-      // } else {
-      //   callback(liveChannel.model);
-      // }
-    },
-  },
   beforeMount() {
-    ClientInstance.init(this.api_key);
-    ClientInstance.registerUserSession(this.userId);
-    this.joinUserToChannel(this.channelId, ChannelType.Standard);
+    ClientInstance.init(this.apiKey);
+
+    ClientInstance.registerUserSession(this.userId, () => {
+      this.joinUserToChannel(this.channelId);
+    });
+  },
+
+  methods: {
+    joinUserToChannel(channelId, type = ChannelType.Standard) {
+      this.channel = null;
+
+      const liveChannel = channelRepo.joinChannel({
+        channelId,
+        type,
+      });
+
+      const callback = (data) => {
+        this.channel = data
+        console.log('the channel has', this.channel.messageCount, 'messages.')
+      };
+
+      liveChannel.once("dataUpdated", callback);
+      liveChannel.model && callback(liveChannel.model);
+
+      liveChannel.once("dataError", async () => {
+        // the error indicates that the displayName wasn't set. in that case:
+        // 1. just run the setDisplayName method
+        await channelRepo.setDisplayName({ channelId, displayName: channelId })
+        // 2. clean the instance
+        liveChannel.dispose()
+        // 3. retry
+        this.joinUserToChannel(channelId, type);
+      })
+    },
   },
 };
 </script>
